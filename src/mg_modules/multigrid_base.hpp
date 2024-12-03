e018387f4dea7a8cfa447c7eb44e39974d748403
Leodsace Sewanou <leodasce.sewanou@ens-lyon.fr>
2024-12-03 09:10:19 +0100

    Addition of an N-dim array

--- src/mg_modules/multigrid_base.hpp
+++ src/mg_modules/multigrid_base.hpp
@@ -5,6 +5,7 @@
 #include <array>
 #include "utilities.hpp"
 #include "Bcondition.hpp"
+#include "ndArray.h"
 
 namespace multigrid
 {
@@ -19,11 +20,21 @@ namespace multigrid
     {
     }
 
+    template <typename T, int dim>
+    inline void restriction_operator(ndArray<T,dim> &corseData, ndArray<T,dim> &fineData)
+    {
+    }
+
+    template <typename T, int dim>
+    inline void interpolation_operator(ndArray<T,dim> &corseData, ndArray<T,dim> &fineData)
+    {
+    }
+
     template <typename T>
     struct Settings
     {
         // T aspectRatio{1};
-        size_t numberOfGrids{8};
+        size_t numberOfGrids{8};  // maxLevel 
         size_t minimumResolution{4};
         T residualTolerance{1e-10};
         size_t maxIterations{400};
@@ -32,15 +43,15 @@ namespace multigrid
     };
 
     template <typename T, int dim>
-    class Stack : public std::vector<std::vector<std::vector<T>>>
+    class Stack : public std::vector<ndArray<T,dim>>
     {
     public:
         Stack(const Settings<T> &s)
         {
             nGrids = s.numberOfGrids;
             mini_resolution = s.minimumResolution;
-            finestLevel(nGrids - 1);
-            resize(nGrids);
+            finestLevel = (nGrids - 1);
+            this->resize(nGrids);
             int nx{mini_resolution}, ny{mini_resolution}, nz{mini_resolution};
             if (dim == 2)
                 nz = 0;
@@ -58,14 +69,15 @@ namespace multigrid
         };
         virtual ~Stack() {};
 
-        const size_t finestLevel;
+        size_t finestLevel;
         static const size_t coarsestLevel = 0;
 
         inline void coarsen(size_t level);
         inline void coarsen(size_t level, std::vector<std::vector<T>> &coarseLevelData);
+        inline void coarsen(size_t level, ndArray<T,dim> &coarseLevelData);
         inline void refine(size_t level);
         inline void refine(size_t level, std::vector<std::vector<T>> &fineLevelData);
-
+        inline void refine(size_t level, ndArray<T,dim> &fineLevelData);
         BoundaryConditions bc;
 
     private:
@@ -86,6 +98,12 @@ namespace multigrid
     }
 
     template <typename T, int dim>
+    inline void Stack<T, dim>::coarsen(size_t level, ndArray<T,dim> &coarseLevelData)
+    {
+        restriction_operator(coarseLevelData, (*this)[level]);
+    }
+
+    template <typename T, int dim>
     inline void Stack<T, dim>::refine(size_t level)
     {
         interpolation_operator((*this)[level], (*this)[level + 1]);
@@ -98,21 +116,31 @@ namespace multigrid
     }
 
     template <typename T, int dim>
+    inline void Stack<T, dim>::refine(size_t level, ndArray<T,dim> &fineLevelData)
+    {
+        interpolation_operator((*this)[level], fineLevelData);
+    }
+    template <typename T, int dim>
     class MultigridBase
     {
     public:
         MultigridBase(const Settings<T> &settings);
         virtual ~MultigridBase() {};
 
-        inline std::vector<std::vector<T>> &get_result();
+        inline ndArray<T,dim> &get_result();
+        // inline std::vector<std::vector<T>> &get_result();
         template <typename U>
         inline void initial_guess(U arg);
-        inline std::vector<std::vector<T>> &source_term();
+        // inline std::vector<std::vector<T>> &source_term();
+        inline ndArray<T,dim> &source_term();
         template <typename U>
         inline void source_term(U arg);
 
         inline void evaluate_operator(size_t level, std::vector<std::vector<T>> &result);
         inline void evaluate_residual(size_t level, std::vector<std::vector<T>> &result);
+
+        inline void evaluate_operator(size_t level, ndArray<T,dim>  &result);
+        inline void evaluate_residual(size_t level, ndArray<T,dim>  &result);
 
         void relax(const size_t level, size_t N);
         void relax(const size_t level, const double tolerance);
@@ -120,18 +148,18 @@ namespace multigrid
         virtual inline void multigrid() {}
         virtual inline void solve() { multigrid(); }
 
-        template <typename T>
+        // template <typename T>
         virtual T differential_operator(size_t, int, int) = 0;
-        template <typename T>
+        // template <typename T>
         virtual T differential_operator(size_t, int, int, int) = 0;
-        template <typename T>
+        // template <typename T>
         virtual void relaxation_updater(size_t, int, int) = 0;
-        template <typename T>
+        // template <typename T>
         virtual void relaxation_updater(size_t, int, int, int) = 0;
 
     protected:
         Stack<T, dim> solution, source; // Grids for solution and source term
-        Stack temp;                     // Extra storage for muligrid solver
+        Stack<T,dim> temp;                     // Extra storage for muligrid solver
         CycleType cycle_type;
         size_t Npre;
         size_t Npost;
@@ -156,7 +184,7 @@ namespace multigrid
                                                                         Npre(settings.Npre),
                                                                         Npost(settings.Npost),
                                                                         residualTolerance(settings.residualTolerance),
-                                                                        maxIter(settings.residualTolerance),
+                                                                        maxIter(settings.maxIterations),
                                                                         isSourceSet(false),
                                                                         isInitialValueSet(false)
     {
@@ -164,19 +192,25 @@ namespace multigrid
         coarsestLevel = solution.coarsestLevel;
         if (dim == 2)
         {
-            nxfine = solution[finestLevel][0];
-            nyfine = solution[finestLevel][0][0];
+            nxfine = solution[finestLevel].size(0);
+            nyfine = solution[finestLevel].size(1);
         }
-        // else if (dim == 3)
-        // {
-        //     nxfine = solution[finestLevel][0];
-        //     nyfine = solution[finestLevel][0][0];
-        //     nzfine = solution[finestLevel][0][0][0];
-        // }
+        else if (dim == 3)
+        {
+            nxfine = solution[finestLevel].size(0);
+            nyfine = solution[finestLevel].size(1);
+            nzfine = solution[finestLevel].size(2);
+        }
     }
 
+    // template <typename T, int dim>
+    // inline std::vector<std::vector<T>> &MultigridBase<T, dim>::get_result()
+    // {
+    //     return solution[finestLevel];
+    // }
+
     template <typename T, int dim>
-    inline std::vector<std::vector<T>> &MultigridBase<T, dim>::get_result()
+    inline ndArray<T, dim> &MultigridBase<T, dim>::get_result()
     {
         return solution[finestLevel];
     }
@@ -189,8 +223,14 @@ namespace multigrid
         isInitialValueSet = true;
     }
 
+    // template <typename T, int dim>
+    // inline std::vector<std::vector<T>> &MultigridBase<T, dim>::source_term()
+    // {
+    //     return source[finestLevel];
+    // }
+
     template <typename T, int dim>
-    inline std::vector<std::vector<T>> &MultigridBase<T, dim>::source_term()
+    inline ndArray<T, dim> &MultigridBase<T, dim>::source_term()
     {
         return source[finestLevel];
     }
@@ -228,6 +268,77 @@ namespace multigrid
                 result[i][j] = source[level][i][j] - differential_operator(level, i, j);
             }
         }
+    }
+
+
+        template <typename T, int dim>
+    inline void MultigridBase<T, dim>::evaluate_operator(size_t level, ndArray<T, dim> &result)
+    {
+        size_t nx = result.size(0);
+        size_t ny = result.size(1);
+        size_t nz = 0;
+        if (dim == 3) nz = result.size(2);
+
+        if(dim == 2)
+        {
+            for (auto i = 0; i < nx; i++)
+            {
+                for (auto j = 0; j < ny; j++)
+                {
+                    result({i,j}) = differential_operator(level, i, j);
+                }
+            }
+        }
+
+        else if (dim == 3)
+        {
+            for(auto k = 0; k < nz; k++)
+            {
+                for(auto j = 0; j < ny; j++)
+                {
+                    for(auto i = 0; i < nx; i++)
+                    {
+                        result({i,j,k}) = differential_operator(level, i, j, k);
+                    }
+                }
+            }
+        }
+       
+    }
+
+    template <typename T, int dim>
+    inline void MultigridBase<T, dim>::evaluate_residual(size_t level, ndArray<T, dim> &result){
+        size_t nx = result.size(0);
+        size_t ny = result.size(1);
+        size_t nz = 0;
+        if (dim == 3) nz = result.size(2);
+
+        if(dim == 2)
+        {
+            for (auto j = 0; j < ny; j++)
+            {
+                for (auto i = 0; i < nx; i++)
+                {
+                    result({i,j}) = source[level]({i,j}) - differential_operator(level, i, j);
+                }
+            }
+        }
+
+        else if (dim==3)
+        {
+            for (auto k = 0; k < nz; k++)
+            {
+                for (auto j = 0; j < ny; j++)
+                {
+                    for(auto i = 0; i < nx; i++)
+                    {
+                         result({i,j,k}) = source[level]({i,j,k}) - differential_operator(level, i, j,k);
+                    }
+                   
+                }
+            }
+        }
+
     }
 
 }
---
+++ src/mg_modules/ndArray.h
@@ -0,0 +1,246 @@
+#ifndef __ND_ARRAY__
+#define __ND_ARRAY__
+
+//================================================
+// @file         ndArray.h
+// @author       Jonathan Hadida
+// @contact      cf https://github.com/Sheljohn/MexArray
+//================================================
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdarg>
+#include <cstdio>
+
+#include <exception>
+#include <stdexcept>
+
+#include <memory>
+#include <algorithm>
+#include <type_traits>
+#include <initializer_list>
+
+// Protect 1D access (comment for faster access)
+#define ND_ARRAY_SAFE_ACCESS
+#ifdef ND_ARRAY_SAFE_ACCESS
+#define ND_ARRAY_PROTECT(k,n) (k % n)
+#else
+#define ND_ARRAY_PROTECT(k,n) k
+#endif
+
+
+
+        /********************     **********     ********************/
+        /********************     **********     ********************/
+
+
+
+/**
+ * Convert nd coordinates to 1d index.
+ * Two main variants are provided:
+ * - Taking an ARRAY as coordinates (size input by template)
+ * - Taking a VA_LIST as a list of coordinate inputs (cf operator() below).
+ */
+template <unsigned N>
+unsigned sub2ind( const unsigned *subs, const unsigned *size, const unsigned *strides )
+{
+    register unsigned ind = 0;
+
+    for (unsigned i = 0; i < N; ++i) 
+        ind += ND_ARRAY_PROTECT(subs[i],size[i]) * strides[i];
+
+    return ind;
+}
+
+template <unsigned N>
+unsigned sub2ind( va_list& vl, const unsigned *size, const unsigned *strides )
+{
+    register unsigned ind = 0;
+
+    for (unsigned i = 1; i < N; ++i) 
+        ind += ND_ARRAY_PROTECT(va_arg(vl,unsigned),size[i]) * strides[i];
+
+    va_end(vl); return ind;
+}
+
+template <> inline unsigned 
+sub2ind<0>( const unsigned*, const unsigned*, const unsigned* )
+    { return 0; }
+template <> inline unsigned 
+sub2ind<1>( const unsigned *subs, const unsigned*, const unsigned* )
+    { return *subs; }
+template <> inline unsigned 
+sub2ind<2>( const unsigned *subs, const unsigned *size, const unsigned* )
+    { return (subs[0] + subs[1]*size[0]); }
+
+// ------------------------------------------------------------------------
+
+/**
+ * Simple singleton.
+ */
+template <typename T> struct singleton { static T instance; };
+template <typename T> T singleton<T>::instance = T();
+
+// ------------------------------------------------------------------------
+
+/**
+ * Dummy deleter functor.
+ * This litterally does nothing to the input pointer; it can be used 
+ * safely with shared pointers for either statically allocated memory 
+ * (eg fixed-size arrays) or externally managed memory (eg Matlab in/out).
+ */
+template <typename T>
+struct no_delete
+{ inline void operator() ( T* ptr ) const {} };
+
+
+
+        /********************     **********     ********************/
+        /********************     **********     ********************/
+
+
+
+/**
+ * n-dimensional array.
+ * NOTE: T can be CONST (underlying elements non-assignable), 
+ * or NON-CONST (underlying elements assignable, suitable for 
+ * owned memory allocation for instance).
+ */
+template <typename T, unsigned N>
+class ndArray
+{
+public:
+
+    typedef T value_type;
+    typedef T* pointer;
+    typedef T& reference;
+
+    typedef typename std::add_const<T>::type const_value;
+    typedef const_value* const_pointer;
+    typedef const_value& const_reference;
+
+    typedef std::shared_ptr<value_type> shared;
+    typedef ndArray<T,N> self;
+
+
+
+    // Constructors
+    ndArray() { reset(); }
+    ndArray( pointer ptr, const unsigned *size, bool manage ) { assign(ptr,size,manage); }
+
+
+    // Copy constructor
+    ndArray( const self& other ) { operator=(other); }
+    self& operator= ( const self& other );
+
+    // Check pointer validity
+    inline bool empty() const { return !((bool) m_data); }
+    inline operator bool() const { return m_data; }
+
+    // Print array dimensions
+    void info() const;
+
+
+
+    // ------------------------------------------------------------------------
+
+
+
+    // Clear contents
+    void clear();
+    void reset();
+
+
+    // Assign a pointer or another instance
+    void assign( pointer ptr, const unsigned *size, bool manage );
+    void assign( const self& other ) { operator=(other); }
+
+
+    // Swap contents with another array
+    void swap( self& other );
+
+
+    // Copy from another array
+    template <typename U>
+    void copy( const ndArray<U,N>& other );
+
+
+
+    // ------------------------------------------------------------------------
+
+
+
+    // 1D access
+    inline reference operator[] ( unsigned n ) const 
+        { return data()[ ND_ARRAY_PROTECT(n,m_numel) ]; }
+
+    // ND access
+    reference operator() ( const unsigned *subs ) const
+        { return data()[ sub2ind<N>(subs, m_size, m_strides) ]; }
+
+    reference operator() ( std::initializer_list<unsigned> subs ) const
+        {
+#ifdef ND_ARRAY_SAFE_ACCESS
+            if ( subs.size() != N ) 
+                throw std::length_error("Invalid coordinates length.");
+#endif
+            return data()[ sub2ind<N>(subs.begin(), m_size, m_strides) ];
+        }
+
+    // Coordinates access
+    reference operator() ( unsigned i, ... ) const
+        { 
+            va_list vl; va_start(vl,i); 
+            return data()[ i+sub2ind<N>(vl, m_size, m_strides) ]; 
+        }
+
+
+    // Access data directly
+    inline const_pointer cdata() const { return m_data.get(); }
+    inline pointer data() const { return m_data.get(); }
+
+    // Iterators
+    inline const_pointer cbegin() const { return data(); }
+    inline const_pointer cend() const { return data() + m_numel; }
+
+    inline pointer begin() const { return data(); }
+    inline pointer end() const { return data() + m_numel; }
+
+
+
+    // ------------------------------------------------------------------------
+
+
+
+    // Dimensions
+    inline const unsigned* size() const { return m_size; }
+    inline unsigned size( unsigned n ) const { return m_size[ n % N ]; }
+    inline const unsigned* strides() const { return m_strides; }
+    inline unsigned stride( unsigned n ) const { return m_strides[ n % N ]; }
+    inline unsigned numel() const { return m_numel; }
+    inline unsigned ndims() const { return N; }
+
+
+
+protected:
+
+    void assign_shared( pointer ptr, bool manage );
+
+    unsigned m_numel;
+    unsigned m_size[N];
+    unsigned m_strides[N];
+
+    shared m_data;
+};
+
+
+
+        /********************     **********     ********************/
+        /********************     **********     ********************/
+
+
+
+// Include implementation
+#include "ndArray.hpp"
+
+#endif
